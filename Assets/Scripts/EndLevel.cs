67fd452b2f73760a0cfcc253c92ad062ff7379de
Coolmoose48 <mika.ahlsten@stonemoose.com>
2021-02-19 17:25:35 +0200

    level work
    Modified Level1 according to new plans. added level2 and end level trigger

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    int levelNumber = 1;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            levelNumber++;
+            SceneManager.LoadScene("level" + levelNumber);
+        }
+    }
+}

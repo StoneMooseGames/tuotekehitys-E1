39f0a2c0323b4c5c52d5de60ebf523621d4ed561
Coolmoose48 <mika.ahlsten@stonemoose.com>
2021-02-19 16:21:26 +0200

    Added spawner
    Add this to the playfield and insert gameobject to the spawner script.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    Vector3 enemyLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        enemyLocation = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y,0);
+        Instantiate(enemy, enemyLocation, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

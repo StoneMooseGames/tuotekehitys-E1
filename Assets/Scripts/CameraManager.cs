60d58ec99ea6676666631c845e1c1c3d69e4da74
Coolmoose48 <mika.ahlsten@stonemoose.com>
2021-02-21 12:46:20 +0200

    Added new Camera Functionality

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Vector3 cameraLocationOffset = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerCamera.transform.position = this.gameObject.transform.position; 
+    }
+}

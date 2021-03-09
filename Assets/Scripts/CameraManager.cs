6e9b25ad5a47de514f7067eab910937e88a218cb
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-03-09 12:31:26 +0200

    cleaned the code

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Camera playerCamera;
-    public Vector3 cameraLocationOffset = new Vector3(0, 0, 0);
+  public Camera playerCamera;
+  public Vector3 cameraLocationOffset = new Vector3(0, 0, 0);
 
-    // Start is called before the first frame update
-    void Start()
+  void Start()
+  {
+    // at start disable Spriterendering for this, because the sprite is there
+    // only to mark the spot for the camera.
+    this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+  }
+
+  void Update() {}
+
+  private void OnTriggerEnter2D( Collider2D other )
+  {
+    // when something enters the collider (Collider is as trigger atm) move camera to the gameobject
+    // where the collider is.
+    if ( other.name == "Player" )
     {
-        //at start disable Spriterendering for this, because the sprite is there
-        //only to mark the spot for the camera.
-        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+      playerCamera.transform.position = this.gameObject.transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //when something enters the collider (Collider is as trigger atm) move camera to the gameobject
-        //where the collider is.
-        if ( collision.ToString().Substring(0,6) == "Player" )
-        {
-          playerCamera.transform.position = this.gameObject.transform.position;
-        }
-    }
+  }
 }

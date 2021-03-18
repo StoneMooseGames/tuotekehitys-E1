f6c8bfebd59e992107b47c8136b306cbec49f27d
Wadezkii <weeti.tiirikainen@hotmail.fi>
2021-03-18 21:07:34 +0200

    spider now follows the player

---
+++ Assets/Scripts/Spider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : MonoBehaviour
+{
+    public float speed;
+    private Transform target;
+
+
+    void Start()
+    {
+
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}

1089115bdcc8ddeae140b97716e42377fc9ccde9
Coolmoose48 <mika.ahlsten@stonemoose.com>
2021-03-25 21:25:14 +0200

    Update Spider.cs
    modified spider behaviour

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -7,6 +7,7 @@ public class Spider : MonoBehaviour
 {
     public float speed;
     public float health;
+    float Maxhealth;
     private Transform target;
     float deathTimer = 3.0f;
     public GameObject deathParticles;
@@ -15,53 +16,34 @@ public class Spider : MonoBehaviour
     public int points;
     Vector2 distanceToPlayer;
     GameObject player;
-    Vector2 triggerDistance = new Vector2(6, 3);
+    public Vector2 triggerDistance = new Vector2(5, 2);
     public Animator animator;
+    bool isAlive = true;
 
 
     void Start()
     {
-        GetComponent<Rigidbody2D>().simulated = true;
-        player = GameObject.Find("Player");
-        gameObject.tag = "enemy";
-        gameObject.name = "Spider";
+        GetComponent<Rigidbody2D>().simulated = true; //start gameobject with no gravity
+        player = GameObject.Find("Player"); //find where player is
+        gameObject.tag = "enemy";    //tagging this as an enemy
+        gameObject.name = "Spider"; //Make sure that gameobjects name si spider if needed in the future
         deathParticles.SetActive(false);  // start with deathparticle sequence turned off
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); //find player
         healthBarLocation = healthBar.GetComponent<RectTransform>(); //store healtbarlocation info
+        Maxhealth = health;
         
     }
     
 
     void Update()
     {
-        if (health <= 0) //if health is below 0
-        {
-            
-            DeathCycle(); //start deathcycle
-        }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-            healthBarLocation.position = transform.position; //make healthbar follow enemy
-            healthBar.GetComponentInChildren<Slider>().value = health; //adjust slider by current healthvalue
-             
-        }
-
-        distanceToPlayer = transform.position - player.transform.position;
-        //this can be checked to see the distance as a 2d vector
-        //Debug.Log(distanceToPlayer);
-        //Check distance in x-axis
-        if (distanceToPlayer.x < triggerDistance.x)
-        {
-            GetComponent<Rigidbody2D>().simulated = true;
-
-        }
-
+        CheckHealth();
+        CheckDistanceToPlayer();
     }
 
     public void DeathCycle() 
     {
-        
+        isAlive = false;
         this.gameObject.transform.rotation = Quaternion.Euler(new Vector3 (180, 0, 0)); // flip object over x-axis by 180 degrees
         if (deathTimer > 0) //while death cycle time is more than 0
         {
@@ -76,10 +58,35 @@ public class Spider : MonoBehaviour
             GameObject.FindGameObjectWithTag("PlayerUI").GetComponent<PlayerUI>().SetPoints(points); //add points accordingly
         }
 
+    }
+    public void CheckHealth()
+    {
+        if (health <= 0) //if health is below 0
+        {
 
+            DeathCycle(); //start deathcycle
+        }
+        else
+        {
+           
+            healthBarLocation.position = transform.position; //make healthbar follow enemy
+            healthBar.GetComponentInChildren<Slider>().value = health; //adjust slider by current healthvalue
+            if(health < Maxhealth && isAlive) transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
 
+    public void CheckDistanceToPlayer()
+    {
+        distanceToPlayer = transform.position - player.transform.position; //check the distance to player
+          
+        //Check distance in x- and y-axis
+        if (distanceToPlayer.x < triggerDistance.x && distanceToPlayer.y < triggerDistance.y && isAlive)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().simulated = true;
 
         }
+    }
 
 
 }
